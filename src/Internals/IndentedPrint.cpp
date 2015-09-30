7fd5ca8aef00de533a7bb2bd3bb3822ce9346446
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-09-30 11:57:33 +0530

    IndentedPrint.cpp

--- src/Internals/IndentedPrint.cpp
+++ src/Internals/IndentedPrint.cpp
@@ -6,24 +6,36 @@
 
 #include "../../include/ArduinoJson/Internals/IndentedPrint.hpp"
 
-using namespace ArduinoJson::Internals;
-
+  using namespace ArduinoJson::Internals;
+  static const int MAX_LEVEL = 15;    // because it's only 4 bits
+  static const int MAX_TAB_SIZE = 7;  // because it's only 3 bits
+  
 size_t IndentedPrint::write(uint8_t c) {
   size_t n = 0;
 
-  if (isNewLine) n += writeTabs();
-
+  if (isNewLine) 
+  try{
+  n += writeTabs();
+  }catch(...){
+    
+  }
+  try{
   n += sink->write(c);
-
-  isNewLine = c == '\n';
-
+  }catch(...){
+    
+  }
   return n;
 }
 
 inline size_t IndentedPrint::writeTabs() {
   size_t n = 0;
 
-  for (int i = 0; i < level * tabSize; i++) n += sink->write(' ');
+  for (int i = 0; i < level * tabSize; i++) 
+  try{
+  n += sink->write(' ');
+  }catch(...){
+    
+  }
 
   return n;
 }
