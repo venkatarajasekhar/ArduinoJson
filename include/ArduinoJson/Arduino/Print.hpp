80d867854d3d5effd9ae79ce7eae1d5eae3d85ff
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-10-01 12:42:36 +0530

    Print.hpp

--- include/ArduinoJson/Arduino/Print.hpp
+++ include/ArduinoJson/Arduino/Print.hpp
@@ -15,10 +15,9 @@
 class Print {
  public:
   virtual ~Print() {}
-
   virtual size_t write(uint8_t) = 0;
 
-  size_t print(const char[]);
+  size_t print(const char *);
   size_t print(double, int = 2);
   size_t print(long);
   size_t println();
